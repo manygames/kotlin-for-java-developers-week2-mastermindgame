33db0be9c0928ba54643c701002dc0b619fc825f
manygames <weberdls@gmail.com>
2018-11-05 18:41:24 -0200

    refactoring to functional style

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -3,62 +3,22 @@ package mastermind
 data class Evaluation(val positions: Int, val letters: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
-    //println("secret: $secret \nguess:  $guess")
-    val (correctLetterAndPositionsAmount, reducedSecret, reducedGuess) =
-            positionsGuessedCorrectly(secret, guess)
 
-    val correctLettersAmount =
-            lettersGuessedCorrectly(reducedSecret, reducedGuess)
+    val positions = secret.zip(guess).count { it.first == it.second }
 
-    return Evaluation(correctLetterAndPositionsAmount, correctLettersAmount)
-}
-
-fun lettersGuessedCorrectly(secret: String, guess: String): Int {
-    //var noDuplicatesGuess = ""
-    //guess.groupBy { letter -> letter }.keys.forEach{ letter -> noDuplicatesGuess+=letter}
-    var correctGuesses = 0
-    var mutableSecret = secret
-    for (ch in guess) {
-        if (mutableSecret.contains(ch)) {
-            mutableSecret = mutableSecret.replaceFirst(ch.toString(), "", true)
-            correctGuesses++
-        }
+    val commonLetters = "ABCDEF".sumBy { ch ->
+        Math.min(secret.count { it == ch }, guess.count { it == ch })
     }
-    return correctGuesses
+    return Evaluation(positions, commonLetters - positions)
 }
 
-fun positionsGuessedCorrectly(secret: String, guess: String): Triple<Int, String, String> {
-    val indexesToRemove = identifyIndexesToRemove(guess, secret)
-    var (reducedSecret, reducedGuess) =
-            reduceStringsRemovingIndexesFrom(secret, guess, indexesToRemove)
-
-    val correctGuesses = indexesToRemove.size
-    return Triple(correctGuesses, reducedSecret, reducedGuess)
+fun main(args: Array<String>) {
+    val result = Evaluation(positions = 1, letters = 1)
+    evaluateGuess("BCDF", "ACEB") eq result
+    evaluateGuess("AAAF", "ABCA") eq result
+    evaluateGuess("ABCA", "AAAF") eq result
 }
-
-private fun identifyIndexesToRemove(guess: String, secret: String): MutableList<Int> {
-    val indexesToRemove = mutableListOf<Int>()
-    for (guessedPair in guess.withIndex()) {
-        val guessedIndex = guessedPair.index
-        val secretLetter = secret.get(guessedIndex)
-        if (guessedPair.value == secretLetter) {
-            indexesToRemove.add(guessedIndex)
-        }
-    }
-    return indexesToRemove
-}
-
-private fun reduceStringsRemovingIndexesFrom(secret: String,
-                                             guess: String,
-                                             indexesToRemove: MutableList<Int>): Pair<String, String> {
-    var currentSecret = secret
-    var currentGuess = guess
-
-    //Need to reverse the list to start removing from higher to lower index
-    //otherwise it could access an unknown index value
-    indexesToRemove.reversed().forEach {
-        currentGuess = currentGuess.replaceRange(it, it + 1, "")
-        currentSecret = currentSecret.replaceRange(it, it + 1, "")
-    }
-    return Pair(currentSecret, currentGuess)
+infix fun <T> T.eq(other: T) {
+    if (this == other) println("OK")
+    else println("Error: $this != $other")
 }
